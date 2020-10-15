59a168c6a9f0e95b6398eb7987e9d4664f0ef2b6
RashadTanjim <rashad.tanjim@northsouth.edu>
2020-10-16 02:45:04 +0600

    RPS changes

--- RPS/src/rps/RPS.java
+++ RPS/src/rps/RPS.java
@@ -5,176 +5,174 @@ import java.util.Random;
 
 public class RPS {
 
-	public static void main(String[] args) {
-		int p1Move = 0; // variable for player1 Move (R/P/S)
-		int p2Move = 0; // variable for player1 Move (R/P/S)
-		int p1Wins = 0; // Initialised variables for Wins keeping PlayerOne,
-		int p2Wins = 0; // Player Two
-		int tie = 0; // Ties
-		int rounds = 0; // number of rounds played
-		String loop = "yes"; // Start loop
-		Scanner input = new Scanner(System.in); // Creates scanner object
+    public static void main(String[] args) {
+        int p1Move = 0; // variable for player1 Move (R/P/S)
+        int p2Move = 0; // variable for player1 Move (R/P/S)
+        int p1Wins = 0; // Initialised variables for Wins keeping PlayerOne,
+        int p2Wins = 0; // Player Two
+        int tie = 0; // Ties
+        int rounds = 0; // number of rounds played
+        String loop = "yes"; // Start loop
+        Scanner input = new Scanner(System.in); // Creates scanner object
 
-		System.out.println("Lets Play Rock Paper Scissors");
-		System.out.println("You know the Rules");
+        System.out.println("Lets Play Rock Paper Scissors");
+        System.out.println("You know the Rules");
 
-		while (loop.equals("yes")) // This loop keeps our game going only while
-									// our string.loop is equal to 'yes'
-		{
-			// p1Move=rgen.nextInt(3)+1;
-			System.out
-					.println("{Player One} please choose -- Rock[r] Paper[p] Scissors[s] Exit[x]");
-			String c1Input = input.nextLine(); // variable for players Move
-												// (R/P/S/X)
-			String c1Move = c1Input.toUpperCase(); // Converts to Upper case in
-													// case user did not
-			System.out
-					.println("{Player Two} please choose -- Rock[r] Paper[p] Scissors[s] Exit[x]");
-			String c2Input = input.nextLine(); // variable for players Move
-												// (R/P/S/X)
-			String c2Move = c2Input.toUpperCase(); // Converts to Upper case in
-													// case user did not
+        while (loop.equals("yes")) // This loop keeps our game going only while
+        // our string.loop is equal to 'yes'
+        {
+            // p1Move=rgen.nextInt(3)+1;
+            System.out
+                    .println("{Player One} please choose -- Rock[r] Paper[p] Scissors[s] Exit[x]");
+            String c1Input = input.nextLine(); // variable for players Move
+            // (R/P/S/X)
+            String c1Move = c1Input.toUpperCase(); // Converts to Upper case in
+            // case user did not
+            System.out
+                    .println("{Player Two} please choose -- Rock[r] Paper[p] Scissors[s] Exit[x]");
+            String c2Input = input.nextLine(); // variable for players Move
+            // (R/P/S/X)
+            String c2Move = c2Input.toUpperCase(); // Converts to Upper case in
+            // case user did not
 
-			// its nicer to use the letters r.p.s and its easier to deal in the
-			// background with numbers
+            // its nicer to use the letters r.p.s and its easier to deal in the
+            // background with numbers
+            if (c1Move.equals("R")) {
+                p1Move = 1;
+            }
+            if (c1Move.equals("P")) {
+                p1Move = 2;
+            }
+            if (c1Move.equals("S")) // This converts pMove to the numeric value
+            // for "Rock"
+            {
+                p1Move = 3;
+            }
+            if (c2Move.equals("R")) {
+                p2Move = 1;
+            }
+            if (c2Move.equals("P")) {
+                p2Move = 2;
+            }
+            if (c2Move.equals("S")) {
+                p2Move = 3;
+            }
+            if (c2Move.equals("X")) // This converts pMove to the numeric value
+            // for "End"
+            {
+                p2Move = 4;
+            }
+            if (c1Move.equals("X")) {
+                p1Move = 4;
+            }
 
-			if (c1Move.equals("R")) {
-				p1Move = 1;
-			}
-			if (c1Move.equals("P")) {
-				p1Move = 2;
-			}
-			if (c1Move.equals("S")) // This converts pMove to the numeric value
-									// for "Rock"
-			{
-				p1Move = 3;
-			}
-			if (c2Move.equals("R")) {
-				p2Move = 1;
-			}
-			if (c2Move.equals("P")) {
-				p2Move = 2;
-			}
-			if (c2Move.equals("S")) {
-				p2Move = 3;
-			}
-			if (c2Move.equals("X")) // This converts pMove to the numeric value
-									// for "End"
-			{
-				p2Move = 4;
-			}
-			if (c1Move.equals("X")) {
-				p1Move = 4;
-			}
+            if (p1Move == p2Move & p1Move != 4 & p2Move != 4) // Takes care of
+            // Ties
+            {
+                System.out.println("Tie Game!");
+                System.out.println("");
+                tie++;
+                rounds++;
+            } else // scoring for non-tie events
+            {
+                if (p1Move == 1 && p2Move == 3) // Computer picks Rock and
+                // player has Scissors - adds
+                // point to Wins and rounds
+                {
+                    System.out
+                            .println("Player1 picked Rock! \n Rock beats Scissors! \n **Player1 Wins! \n**");
+                    p1Wins++;
+                    rounds++;
+                }
 
-			if (p1Move == p2Move & p1Move != 4 & p2Move != 4) // Takes care of
-																// Ties
-			{
-				System.out.println("Tie Game!");
-				System.out.println("");
-				tie++;
-				rounds++;
-			} else // scoring for non-tie events
-			{
-				if (p1Move == 1 && p2Move == 3) // Computer picks Rock and
-												// player has Scissors - adds
-												// point to Wins and rounds
-				{
-					System.out
-							.println("Player1 picked Rock! \n Rock beats Scissors! \n **Player1 Wins! \n**");
-					p1Wins++;
-					rounds++;
-				}
+                if (p1Move == 1 && p2Move == 2) // Computer picks Rock and
+                // player has Paper - adds point
+                // to Wins and rounds
+                {
+                    System.out
+                            .println("Player1 picked Rock! \n Paper beats Rock! \n **Player2 Wins!**");
+                    System.out.println("");
+                    p2Wins++;
+                    rounds++;
+                }
 
-				if (p1Move == 1 && p2Move == 2) // Computer picks Rock and
-												// player has Paper - adds point
-												// to Wins and rounds
-				{
-					System.out
-							.println("Player1 picked Rock! \n Paper beats Rock! \n **Player2 Wins!**");
-					System.out.println("");
-					p2Wins++;
-					rounds++;
-				}
+                if (p1Move == 2 && p2Move == 3) // Computer picks Paper and
+                // player has Scissors - adds
+                // point to Wins and rounds
+                {
+                    System.out
+                            .println("Player1 picked Paper! \n Scissors beats Paper! \n **Player2 Wins!**\n");
+                    p2Wins++;
+                    rounds++;
+                }
 
-				if (p1Move == 2 && p2Move == 3) // Computer picks Paper and
-												// player has Scissors - adds
-												// point to Wins and rounds
-				{
-					System.out
-							.println("Player1 picked Paper! \n Scissors beats Paper! \n **Player2 Wins!**\n");
-					p2Wins++;
-					rounds++;
-				}
+                if (p1Move == 2 && p2Move == 1) // Computer picks Paper and
+                // player has Rock - adds point
+                // to Wins and rounds
+                {
+                    System.out
+                            .println("Player1 picked Paper! \n Paper beats Rock!\n **Player1 Wins!** \n");
+                    p1Wins++;
+                    rounds++;
+                }
 
-				if (p1Move == 2 && p2Move == 1) // Computer picks Paper and
-												// player has Rock - adds point
-												// to Wins and rounds
-				{
-					System.out
-							.println("Player1 picked Paper! \n Paper beats Rock!\n **Player1 Wins!** \n");
-					p1Wins++;
-					rounds++;
-				}
+                if (p1Move == 3 && p2Move == 1) // Computer picks Scissors and
+                // player has Rock - adds point
+                // to Wins and rounds
+                {
+                    System.out
+                            .println("Player1 picked Scissors! \n Rock beats Scissors! \n **Player2 Wins!**");
 
-				if (p1Move == 3 && p2Move == 1) // Computer picks Scissors and
-												// player has Rock - adds point
-												// to Wins and rounds
-				{
-					System.out
-							.println("Player1 picked Scissors! \n Rock beats Scissors! \n **Player2 Wins!**");
+                    System.out.println("");
+                    p2Wins++;
+                    rounds++;
+                }
 
-					System.out.println("");
-					p2Wins++;
-					rounds++;
-				}
+                if (p1Move == 3 && p2Move == 2) // Computer picks Scissors and
+                // player has Paper - adds point
+                // to Wins and rounds
+                {
+                    System.out
+                            .println("Player1 picked Scissors! \n Scissors beats Paper! \n **Player1 Wins!** \n");
+                    p1Wins++;
+                    rounds++;
+                }
+                if (p1Move == 4) // Computer picks Scissors and player has Paper
+                // - adds point to Wins and rounds
+                {
+                    System.out.println("Game Over, Player One Quit");
+                    System.out.println("THE FINAL SCORES AFTER " + rounds
+                            + " ROUNDS: \n PLAYER 1\tPLAYER2\tTies");
+                    System.out.println(" " + p1Wins + "\t   " + p2Wins
+                            + "\t\t " + tie);
+                    loop = "no"; // terminates the while loop keeping the game
+                    // going
+                }
+                if (p2Move == 4) // Computer picks Scissors and player has Paper
+                // - adds point to Wins and rounds
+                {
+                    System.out.println("Game Over, Player Two Quit");
+                    System.out.println("THE FINAL SCORES AFTER " + rounds
+                            + " ROUNDS: \n PLAYER 1\tPLAYER2\tTies");
+                    System.out.println(" " + p1Wins + "\t" + p2Wins + "\t\t "
+                            + tie);
+                    loop = "no"; // terminates the while loop keeping the game
+                    // going
+                }
+                if (p1Move == 4 & p2Move == 4) // Computer picks Scissors and
+                // player has Paper - adds point
+                // to Wins and rounds
+                {
+                    System.out.println("Game Over, You both Quit");
+                    System.out.println("THE FINAL SCORES AFTER " + rounds
+                            + " ROUNDS: PLAYER 1\tPLAYER2\tTies\n");
+                    System.out.println(" " + p1Wins + "\t" + p2Wins + "\t\t "
+                            + tie);
+                    loop = "no"; // terminates the while loop keeping the game
+                    // going
+                }
+            }
 
-				if (p1Move == 3 && p2Move == 2) // Computer picks Scissors and
-												// player has Paper - adds point
-												// to Wins and rounds
-				{
-					System.out
-							.println("Player1 picked Scissors! \n Scissors beats Paper! \n **Player1 Wins!** \n");
-					p1Wins++;
-					rounds++;
-				}
-				if (p1Move == 4) // Computer picks Scissors and player has Paper
-									// - adds point to Wins and rounds
-				{
-					System.out.println("Game Over, Player One Quit");
-					System.out.println("THE FINAL SCORES AFTER " + rounds
-							+ " ROUNDS: \n PLAYER 1\tPLAYER2\tTies");
-					System.out.println(" " + p1Wins + "\t   " + p2Wins
-							+ "\t\t " + tie);
-					loop = "no"; // terminates the while loop keeping the game
-									// going
-				}
-				if (p2Move == 4) // Computer picks Scissors and player has Paper
-									// - adds point to Wins and rounds
-				{
-					System.out.println("Game Over, Player Two Quit");
-					System.out.println("THE FINAL SCORES AFTER " + rounds
-							+ " ROUNDS: \n PLAYER 1\tPLAYER2\tTies");
-					System.out.println(" " + p1Wins + "\t" + p2Wins + "\t\t "
-							+ tie);
-					loop = "no"; // terminates the while loop keeping the game
-									// going
-				}
-				if (p1Move == 4 & p2Move == 4) // Computer picks Scissors and
-												// player has Paper - adds point
-												// to Wins and rounds
-				{
-					System.out.println("Game Over, You both Quit");
-					System.out.println("THE FINAL SCORES AFTER " + rounds
-							+ " ROUNDS: PLAYER 1\tPLAYER2\tTies\n");
-					System.out.println(" " + p1Wins + "\t" + p2Wins + "\t\t "
-							+ tie);
-					loop = "no"; // terminates the while loop keeping the game
-									// going
-				}
-			}
-
-		}
-	}
+        }
+    }
 }
-
--- RPS/src/rps/VerySimpleChatServer.java
+++ RPS/src/rps/VerySimpleChatServer.java
@@ -5,116 +5,115 @@ import java.net.*;
 import java.util.*;
 
 public class VerySimpleChatServer {
-	// can add values to ArrayList with .add
-	// below we use ""clientOutputStreams.add(writer);""
-	// we add writer...we can also use
-	// clientOutputStreams.add(indexOfTheValueYouWantToGet)
-	ArrayList clientOutputStreams;
+    // can add values to ArrayList with .add
+    // below we use ""clientOutputStreams.add(writer);""
+    // we add writer...we can also use
+    // clientOutputStreams.add(indexOfTheValueYouWantToGet)
 
-	// this is our thread constructor that needs to be passed an object
-	// runnable interface is in java.lang package
-	public class ClientHandler implements Runnable {
-		BufferedReader reader;
-		Socket sock;
+    ArrayList clientOutputStreams;
 
-		public ClientHandler(Socket clientSocket) {
-			try {
-				sock = clientSocket;
-				InputStreamReader isReader = new InputStreamReader(
-						sock.getInputStream());
-				reader = new BufferedReader(isReader);
+    // this is our thread constructor that needs to be passed an object
+    // runnable interface is in java.lang package
+    public class ClientHandler implements Runnable {
 
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
+        BufferedReader reader;
+        Socket sock;
 
-		// ok so Runnable has only one method to implement:
-		// this is where i put the game I thinks
-		// method that goes at bottom of new stack.
-		// also it seems its an interface
-		// //GIVE our thread a job
-		public void run() {
+        public ClientHandler(Socket clientSocket) {
+            try {
+                sock = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(
+                        sock.getInputStream());
+                reader = new BufferedReader(isReader);
 
-			String message;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
 
-			try {
-				// InputStreamReader isReader = new
-				// InputStreamReader(sock.getInputStream());
-				// reading in from socket i thinks
-				// reader = new BufferedReader(isReader);
-				//
-				while ((message = reader.readLine()) != null) {
+        // ok so Runnable has only one method to implement:
+        // this is where i put the game I thinks
+        // method that goes at bottom of new stack.
+        // also it seems its an interface
+        // //GIVE our thread a job
+        public void run() {
 
-					// System.out.println("arse " + message);
-					// RPS game = new RPS();
-					tellEveryone(message);
-				}
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
-	}
+            String message;
 
-	public static void main(String[] args) {
-		new VerySimpleChatServer().start();
-	}
+            try {
+                // InputStreamReader isReader = new
+                // InputStreamReader(sock.getInputStream());
+                // reading in from socket i thinks
+                // reader = new BufferedReader(isReader);
+                //
+                while ((message = reader.readLine()) != null) {
 
-	public void start() {
-		clientOutputStreams = new ArrayList();
-		try {
-			ServerSocket serverSock = new ServerSocket(5000);
+                    // System.out.println("arse " + message);
+                    // RPS game = new RPS();
+                    tellEveryone(message);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 
-			// into the permanent loop. waiting for (and servicing)client
-			// requests
-			while (true) {
+    public static void main(String[] args) {
+        new VerySimpleChatServer().start();
+    }
 
-				Socket clientSocket = serverSock.accept();
+    public void start() {
+        clientOutputStreams = new ArrayList();
+        try {
+            ServerSocket serverSock = new ServerSocket(5000);
 
-				PrintWriter writer = new PrintWriter(
-						clientSocket.getOutputStream());
+            // into the permanent loop. waiting for (and servicing)client
+            // requests
+            while (true) {
 
-				// writer.println("Hello" + "");
+                Socket clientSocket = serverSock.accept();
 
-				clientOutputStreams.add(writer);
-				// make the thread object (the worker) and give it a runnable
-				// (the job)
-				// "new (ClientHandler(clientSocket)) is the "threadjob"
-				// thread instance created put not started //
-				Thread t = new Thread(new ClientHandler(clientSocket));
-				// once this is started you go from just a thread to new
-				// thread of execution
-				// when new thread starts up, it takes the runnable objects
-				// run()
-				// method and puts it on the bottom of the the new threads stack
-				// so we shall bring our new ClientHandler(clientSocket) to
-				// run()
-				// //start tread, it moves into runnable state
-				// means thread is ready to be run.
-				t.start();
-				System.out.println("got a connection");
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+                PrintWriter writer = new PrintWriter(
+                        clientSocket.getOutputStream());
 
-	//
-	public void tellEveryone(String message) {
-		Iterator it = clientOutputStreams.iterator();
+                // writer.println("Hello" + "");
+                clientOutputStreams.add(writer);
+                // make the thread object (the worker) and give it a runnable
+                // (the job)
+                // "new (ClientHandler(clientSocket)) is the "threadjob"
+                // thread instance created put not started //
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                // once this is started you go from just a thread to new
+                // thread of execution
+                // when new thread starts up, it takes the runnable objects
+                // run()
+                // method and puts it on the bottom of the the new threads stack
+                // so we shall bring our new ClientHandler(clientSocket) to
+                // run()
+                // //start tread, it moves into runnable state
+                // means thread is ready to be run.
+                t.start();
+                System.out.println("got a connection");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-		while (it.hasNext()) {
-			try {
+    //
+    public void tellEveryone(String message) {
+        Iterator it = clientOutputStreams.iterator();
 
-				PrintWriter writer = (PrintWriter) it.next();
+        while (it.hasNext()) {
+            try {
 
-				writer.println(message);
-				writer.flush();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
-	}
+                PrintWriter writer = (PrintWriter) it.next();
+
+                writer.println(message);
+                writer.flush();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
-
-
